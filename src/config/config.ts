27a8f17263d418a252d37b062d78a3130b30ff79
apestogetherstrongdoc <136205605+apestogetherstrongdoc@users.noreply.github.com>
2023-06-12 18:28:36 -0400

    Testing

--- src/config/config.ts
+++ src/config/config.ts
@@ -21,23 +21,34 @@ Here are two examples:
 
 const unlockables: Unlockable[] = [
   {
-    cid: "bafybeiehgpaip4f7jafzf7imgannx3nnv3ubaiwp6ph56mlyzijpqxi45m", // HTML blog example
+    cid: "bafybeif42jlthaiy7m7frxp6rsyfeqvxlmr2cpio4oiqfsjthurc6cxloa", // ATS Video
     nftId: [
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' to unlock this.
+      "0xde2839b3dea14689613bb52ea320e660f1071b582a2481c6bc577fce7ad671b0",
+    ], // ATS Video
   },
   {
-    cid: "bafybeihx5eacyxeydcpvudwxa242rnjhn67femy46gzas5d2djb24ti5mi", // MP4 video example
+    cid: "bafybeibbfa7zxtjxy56kw345n3r6wpdwrln3dn7po623lwcmzf7l576twa", // Deleted Scene with u/Buttfarm69 Video
     nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-    ], // You need to own 'GM #2' to unlock this.
+      "0xde2839b3dea14689613bb52ea320e660f1071b582a2481c6bc577fce7ad671b0",
+    ], // Deleted Scene with u/Buttfarm69 Video
   },
   {
-    cid: "bafybeihhx5v3saq3b7n55ub5q3atuw2udbqc5ictkv2ih7vd3hxptu22nu", // Web game example
+    cid: "bafybeifp4kct5qzgprjmt7jyi5jf5hdksp7vezpet6qkzpc5www56w2tnm", // ATS Video Game (iFrame NJJ Full Version)
     nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' & 'GM #2' to unlock this.
+      "0xde2839b3dea14689613bb52ea320e660f1071b582a2481c6bc577fce7ad671b0",
+    ], // ATS Video Game (iFrame NJJ Full Version)
+  },
+  {
+    cid: "bafybeifxxpoeoxzv4dx5jdsjc6l6f3n2qd4ibpeiwlwj4tjcngcbat7heu", // VIP Placeholder 01
+    nftId: [
+      "0xde2839b3dea14689613bb52ea320e660f1071b582a2481c6bc577fce7ad671b0",
+    ], // VIP Placeholder 01
+  },
+  {
+    cid: "bafybeic2g73iaqlqjsgtmadlsiyqah23qnjsjlevs4byxyqo7zkhvj4duq", // VIP Placeholder 02
+    nftId: [
+      "0xde2839b3dea14689613bb52ea320e660f1071b582a2481c6bc577fce7ad671b0",
+    ], // VIP Placeholder 02
   },
 ];
 
